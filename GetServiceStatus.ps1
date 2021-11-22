71a9a35607ccae1ae738c0af92308565c4ba6be5
Igor Barkov <ibarkov@hrsinternational.com>
2021-11-22 21:49:24 +0300

    GSS-add coment

--- GetServiceStatus.ps1
+++ GetServiceStatus.ps1
@@ -1,21 +1,21 @@
-#Service names
+############ Service names ###############
 $services = @('Spooler','TeamViewer')
 
-#email credentials
-#Send Email From:
+########### email credentials ############
 $emailPass = 'password'
 $emailName = 'test'
 
-#email settings
+########## email settings ################
 $emailSrcAdress = 'test@gmail.com'
 $emailDstAddress = 'test@outlook.com'
 $smtpServer = "smtp.gmail.com"
 $smtpPort = 587
+##########################################
 
 $secpasswd = ConvertTo-SecureString $emailPass -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ($emailName, $secpasswd)
 
-#Send_message
+######## Send_message ######################
 Send-MailMessage -From $emailSrcAdress -To $emailDstAddress -Subject "Service status on $env:computername $(Get-Date -Format g)" -Body ($services | ForEach-Object {Get-Service -Name $_} | Format-Table -Wrap | Out-String) -SmtpServer $smtpServer -Port $smtpPort -credential $mycreds -UseSsl
 
 
